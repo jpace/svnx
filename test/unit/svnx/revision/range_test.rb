a4e8e5215c106dfac96ff25ee789920512088afe
Jeff Pace <jeugenepace@gmail.com>
2013-08-24 17:37:08 -0400

    Added Range test from PVN.

---
+++ test/unit/svnx/revision/range_test.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby -w
+# -*- ruby -*-
+
+require 'tc'
+require 'svnx/log/entries'
+require 'svnx/revision/range'
+require 'resources'
+
+module SVNx::Revision
+  class RangeTestCase < SVNx::TestCase
+    def test_init
+      rr = Range.new '143:199'
+      assert_equal '143', rr.from.to_s
+      assert_equal '199', rr.to.to_s
+      assert_equal '143:199', rr.to_s
+    end
+
+    def test_to_working_copy
+      rr = Range.new '143'
+      assert_equal '143', rr.from.to_s
+      assert_nil rr.to
+      assert_equal '143', rr.to_s
+    end
+  end
+end
