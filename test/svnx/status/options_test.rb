1214ddede0fa0280cad6e2b49dae7563e69f23a9
Jeff Pace <jeugenepace@gmail.com>
2017-06-14 09:23:13 -0400

    Parameterize tests of status options.

--- test/svnx/status/options_test.rb
+++ test/svnx/status/options_test.rb
@@ -3,8 +3,11 @@
 
 require 'svnx/status/options'
 require 'svnx/options/tc'
+require 'paramesan'
 
 class Svnx::Status::OptionsTest < Svnx::Options::TestCase
+  extend Paramesan
+  
   def options_class
     Svnx::Status::Options
   end
@@ -16,36 +19,20 @@ class Svnx::Status::OptionsTest < Svnx::Options::TestCase
     }
     assert_options defexpected
   end
-  
-  def test_assign_paths
-    assert_assign paths: [ "a/b", "c/d" ]
-  end 
-  
-  def test_assign_url
-    assert_assign url: "p://a/b"
+
+  param_test [
+    { paths: [ "a/b", "c/d" ] },
+    { url: "p://a/b" }
+  ].each do |vals|
+    assert_assign vals
   end
 
-  def create_options optargs = Hash.new
-    Svnx::Status::Options.new optargs
-  end
-
-  def create_args options
-    Svnx::Status::Args.new options
-  end
-  
-  def test_to_args_default
-    assert_to_args Array.new
-  end
-  
-  def test_to_args_url
-    assert_to_args [ "p://abc" ], url: "p://abc"
-  end
-
-  def test_to_args_paths_single
-    assert_to_args [ "a/b" ], paths: [ "a/b" ]
-  end
-
-  def test_to_args_paths_multiple
-    assert_to_args [ "a/b", "c/d" ], paths: [ "a/b", "c/d" ]
+  param_test [
+    [ Array.new, Hash.new ],
+    [ [ "p://abc" ], url: "p://abc" ],
+    [ [ "a/b" ], paths: [ "a/b" ] ],
+    [ [ "a/b", "c/d" ], paths: [ "a/b", "c/d" ] ],
+  ].each do |exp, vals|
+    assert_to_args exp, vals
   end
 end
