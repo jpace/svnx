83950d589a100cb8fbff9568dcaca531be640e14
Jeff Pace <jeugenepace@gmail.com>
2017-12-17 11:55:41 -0500

    Clean up delegator code from project to commands.

--- lib/svnx/project.rb
+++ lib/svnx/project.rb
@@ -1,26 +1,19 @@
 #!/usr/bin/ruby -w
 # -*- ruby -*-
 
-require 'logue/loggable'
-
 module Svnx
 end
 
 # A low-level wrapper around the Svnx commands, converting arguments (svnx/<command>/options) into
 # entries (svnx/<command>/entry) or output. Enhances the low level functionality.
 
-class Svnx::Project
-  include Logue::Loggable
-  
+class Svnx::Project  
   attr_reader :dir
   
   def initialize dir: nil, url: nil, cls: nil
     @dir = dir
     @url = url
     @cls = cls
-    debug "dir: #{dir}"
-    debug "url: #{url}"
-    debug "cls: #{cls}"
   end
 
   def where
@@ -38,71 +31,34 @@ class Svnx::Project
     info.path
   end
 
-  def run_command cmdcls, cmdargs, cls: @cls
-    debug "cmdargs: #{cmdargs}"
-    # debug "args: #{args}"
-    cmd = cmdcls.new cmdargs, cls: cls
-    cmd.respond_to?(:entries) ? cmd.entries : cmd.output
-  end
+  def self.add_delegator cmd
+    require "svnx/#{cmd}/command"
+    require "svnx/#{cmd}/options"
 
-  def self.add_command_delegator name, takes_multiple_paths
-    require "svnx/#{name}/command"
-
-    pathargs = takes_multiple_paths ? "paths: [ @dir ]" : "path: @dir"
+    initargs = Hash[url: "@url", path: "@dir", paths: "[ @dir ]"]
+    optcls   = Kernel.const_get "Svnx::#{cmd.to_s.capitalize}::Options"
+    opts     = optcls.new Hash.new
+    fields   = opts.fields.keys
+    params   = fields.collect { |key| key.to_s + ": " + (initargs[key] || "nil") }.join ", "
+    cmdargs  = fields.collect { |key| key.to_s + ": " + key.to_s }.join ", "
     
-    unless NOURL.include? name
-      pathargs << ", url: @url"
-    end
+    src = Array.new.tap do |a|
+      a << "def #{cmd} #{params}, cls: @cls"
+      a << "  cmd = Svnx::#{cmd.to_s.capitalize}::Command.new({ #{cmdargs} }, cls: cls)"
+      a << "  cmd.respond_to?(:entries) ? cmd.entries : cmd.output"
+      a << "end"
+    end.join "\n"
     
-    args = [
-      "Svnx::#{name.to_s.capitalize}::Command",
-      "{ " + pathargs + " }",
-      "**cmdargs"
-    ]
-
-    src = [
-      "def #{name} cls: nil, **cmdargs",
-      "  debug \"cmdargs: \#{cmdargs}\"",
-      "  run_command " + args.join(", ") + ", cls: @cls",
-      "end"
-    ].join("\n")
-
-    puts "src: #{src}"
-
-    module_eval src
+    class_eval src
   end
 
-  NOURL = [ :diff, :update, :merge ]
-  
-  add_command_delegator :info,    false
-  
-  add_command_delegator :update,  true
-  
-  add_command_delegator :commit,  true
-  # add_command_delegator :log,     false
-  
-  add_command_delegator :diff,    true
-  add_command_delegator :propset, false
-  add_command_delegator :propget, false
-
-  def self.add_log_method
-    require "svnx/log/command"
-
-    src = ""
-    src << "def log limit: nil, verbose: nil, revision: nil, url: nil, path: nil, cls: nil\n"
-    src << "  cmd = Svnx::Log::Command.new({ limit: limit, verbose: verbose, revision: revision, url: url, path: path }, cls: cls)\n"
-    src << "  cmd.respond_to?(:entries) ? cmd.entries : cmd.output\n"
-    src << "end\n"
-
-    module_eval src
-  end
-
-  def log limit: nil, verbose: nil, revision: nil, url: nil, path: nil, cls: nil
-    require "svnx/log/command"
-    
-    cmd = Svnx::Log::Command.new({ limit: limit, verbose: verbose, revision: revision, url: url, path: path }, cls: cls)
-    cmd.respond_to?(:entries) ? cmd.entries : cmd.output
-  end
+  add_delegator :commit
+  add_delegator :diff
+  add_delegator :info
+  add_delegator :log
+  add_delegator :propget
+  add_delegator :propset
+  add_delegator :update
 
   def to_s
     where.to_s
