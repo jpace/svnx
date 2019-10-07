1ad8825954bcfbed120f0f6c47b91e2875beb61c
Jeff Pace <jpace@softwareag.com>
2019-10-07 14:18:51 -0400

    Change diff to non-xml

--- lib/svnx/diff/command.rb
+++ lib/svnx/diff/command.rb
@@ -7,12 +7,19 @@ require 'svnx/diff/parser'
 
 module Svnx::Diff
   class Command < Svnx::Base::Command
+    noncaching
+    nonxml
+    
     attr_reader :entries
     
     def initialize cmdopts, cmdlinecls: Svnx::Base::CommandLine
       super cmdopts, cmdlinecls: cmdlinecls, caching: true
       if @output
+        debug "@output: #{@output}"
+        debug "@entries: #{@entries}"
         @entries = Svnx::Diff::Parser.new.parse_all_output @output.dup
+      else
+        debug "Svnx::Diff::Command#init: no output"
       end
     end
   end
